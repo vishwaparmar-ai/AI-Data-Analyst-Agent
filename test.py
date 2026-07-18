1011748bdb3a284d06ad4bd2ca56e68d0c93e1b4
vishwa <vishwaparmar1001@gmail.com>
2026-07-18 14:36:30 +0530

    Stop tracking test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from backend.agents.data_understanding_agent import DataUnderstandingAgent
-
-cleaner = DataUnderstandingAgent()
-
-df = cleaner.read_dataset("uploads/ecommerce_data.csv")
-
-report = cleaner.analyze_dataset(df)
-
-print(report)
